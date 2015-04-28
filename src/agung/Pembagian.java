321c3e108384ba24a5cef20574d2460d447cd361
SBC4 <SBC4@SBC04>
2015-04-28 14:10:33 +0700

    commit kedua

---
+++ src/agung/Pembagian.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package agung;
+
+/**
+ *
+ * @author SBC4
+ */
+public class Pembagian {
+    public static void main(String[] args) {
+        int a=27,b=12;
+        
+        System.out.println(a+" : "+b+" = "+(a/b));
+    }
+}
